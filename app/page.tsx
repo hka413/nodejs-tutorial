2133fdf48b820e4924f2c6fa3ef058e4e0a6a8b7
Husain Kahil <husain@moments.capital>
2025-03-09 16:45:41 -0700

    fixed file structure

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is a basic home page built with React and TypeScript.</p>
+        </div>
+    );
+};
+
+export default HomePage;
